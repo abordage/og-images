0de969f9053bda3b3073de9683dcb59a20909aea
Pavel Bychko <abordage.dev@gmail.com>
2022-05-27 08:51:18 +0300

    update config

--- src/OpenGraphImages.php
+++ src/OpenGraphImages.php
@@ -136,7 +136,7 @@ class OpenGraphImages
         |            "bottom-left", "bottom-center", "bottom-right"
         |
         */
-        'app_name_position' => 'center-bottom',
+        'app_name_position' => 'bottom-center',
 
         /*
         |--------------------------------------------------------------------------
